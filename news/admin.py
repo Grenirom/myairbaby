b57212a41805324501548c838aac0c90b02c0afa
Nick <ngrebnev17@gmail.com>
2023-10-30 19:20:34 +0600

    added them to admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from news.models import New, Comment, Rate
 
-# Register your models here.
+admin.site.register(New)
+admin.site.register(Comment)
+admin.site.register(Rate)
